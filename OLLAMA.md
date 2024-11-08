88079e973b51f163fe4f214d5360ad0ea09a8288
Rodrigo Linfati <rodrigo@linfati.cl>
2024-11-08 17:15:42 -0300

    v3

--- OLLAMA.md
+++ OLLAMA.md
@@ -6,10 +6,6 @@
 
 ## ollama pull model
 - sudo podman exec  --interactive --tty LLM-ollama bash
-- ollama run llama3.1:8b-instruct-q5_K_M
-- ollama run gemma2:9b-instruct-q5_K_M
-- ollama run mistral-nemo:12b-instruct-2407-q5_K_M
-- ollama run mistral:7b-instruct-v0.3-q5_K_M
-- ollama run qwen2:7b-instruct-q5_K_M
-- ollama run phi3:14b-medium-4k-instruct-q5_K_M
-
+- ollama pull llama3.1:8b-instruct-q5_K_M
+- ollama pull mistral-nemo:12b-instruct-2407-q5_K_M
+- ollama pull gemma2:9b-instruct-q5_K_M
--- lab0-refresh.sh
+++ lab0-refresh.sh
@@ -4,12 +4,12 @@ set -x
 
 sudo podman pull docker.io/rlinfati/containerfile-lab0:grbts-999
 sudo podman pull docker.io/rlinfati/containerfile-lab0:ikev2-tls-999
-sudo podman pull docker.io/rlinfati/containerfile-lab0:samba-999
-sudo podman pull docker.io/rlinfati/containerfile-lab0:slurm-999
-sudo podman pull docker.io/rlinfati/containerfile-lab0:vlmcsd-999
 
 sudo podman image prune --force
 
+sudo podman image ls -a
+sudo podman container ls
+sudo systemctl start podman-auto-update
 sudo podman image ls -a
 sudo podman container ls
 
