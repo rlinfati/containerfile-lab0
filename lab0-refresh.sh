5def8b6aa8f5e040f2e4fb3bfc9b9543231aae92
Rodrigo Linfati <rodrigo@linfati.cl>
2025-08-25 18:23:47 -0400

    c

--- lab0-refresh.sh
+++ lab0-refresh.sh
@@ -6,9 +6,9 @@ sudo podman pull docker.io/rlinfati/containerfile-lab0:grbts
 sudo podman pull docker.io/rlinfati/containerfile-lab0:ikev2-tls
 
 sudo podman image prune --force
-
 sudo podman image ls -a
 sudo podman container ls
+
 sudo systemctl start podman-auto-update
 sudo podman image ls -a
 sudo podman container ls
